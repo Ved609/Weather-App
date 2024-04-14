50029851a8740a38aa317b72e4205f3fa5a60933
Ved Tusharbhai Patel <131395790+Ved609@users.noreply.github.com>
2024-04-14 20:14:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+// JavaScript code
+const api = {
+  key: "fcc8de7015bbb202209bbf0261babf4c",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(${api.base}weather?q=${query}&units=metric&APPID=${api.key})
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('City not found');
+      }
+      return response.json();
+    })
+    .then(displayResults)
+    .catch(error => {
+      console.error(error.message);
+      alert(error.message);
+    });
+}
+
+function displayResults(weather) {
+  let city = document.querySelector('.location .city');
+  city.textContent = ${weather.name}, ${weather.sys.country};
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.textContent = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = ${Math.round(weather.main.temp)}<span>°c</span>;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.textContent = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.textContent = ${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c;
+}
+
+function dateBuilder(d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return ${day} ${date} ${month} ${year};
+}
